b423c351a88739a285d4aaef42267b64acae3f12
Francis Dela Cruz <50617340+fdc981@users.noreply.github.com>
2023-07-02 20:51:53 +0930

    Replace global2instance loader with crude loader
    As p5-global2instance does not handle variable assignments with p5 functions on the right hand side, this commit adds a crude loader for the time being.

--- p5-global-loader.js
+++ p5-global-loader.js
@@ -1,4 +1,5 @@
 const global2instance = require('p5-global2instance');
+const p5func = require('p5-global2instance/list-p5func');
 
 /**
  * Converts a p5 script in global mode to instance mode.
@@ -11,4 +12,39 @@ function p5Global2InstanceLoader(content, map, meta) {
   return global2instance(content);
 }
 
-module.exports = p5Global2InstanceLoader;
+/**
+ * Converts a p5 script in global mode to instance mode crudely.
+ *
+ * Since this function replaces substrings matching p5 globals, the input
+ * should be carefully checked beforehand.
+ *
+ * @param {string|Buffer} content Content of the resource file containing p5 data.
+ * @param {object} [map] Unused.
+ * @param {any} [meta] Unused.
+ */
+function crudeLoader(content, map, meta) {
+  for (let expr of p5func['p5scopeFuncs']) {
+    content = content.replaceAll("function " + expr + "() {",
+                                 "$_p." + expr + " = function() {");
+  }
+
+  for (let expr of p5func['p5exprs']) {
+    content = content.replaceAll(new RegExp("(?<!\$\_p\.)" + expr, "g"),
+                                 "$_p." + expr);
+  }
+  content = content.replaceAll(new RegExp("(\\$_p\\.)+", "g"), "$_p.");
+
+  content = `
+  import p5 from 'p5';
+
+  let sketch = function($_p) {
+    ${content}
+  }
+
+  let myp5 = new p5(sketch);
+  `
+
+  return content;
+}
+
+module.exports = crudeLoader;
