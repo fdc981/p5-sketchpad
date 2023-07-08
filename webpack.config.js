184e2d1365049df60b01290cf91f8a474ff65217
Francis Dela Cruz <50617340+fdc981@users.noreply.github.com>
2023-07-08 13:14:10 +0930

    Add 'sketch' environment variable to allow preview of other .p5.js files

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,58 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  entry: {
-    sketch: {
-      import: './src/sketch.p5.js',
-    },
-  },
-  performance: {
-    hints: false
-  },
-  mode: "production",
-  devServer: {
-    static: './dist',
-    hot: true
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      title: "p5 sketch"
-    })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.p5.js$/,
-        use: [
-          {
-            loader: path.resolve('p5-global-loader.js')
-          }
-        ]
+module.exports = (env) => {
+  const sketchName = env.sketch === undefined ? "sketch" : env.sketch;
+
+  return {
+    entry: {
+      sketch: {
+        import: `./src/${sketchName}.p5.js`,
       },
+    },
+    performance: {
+      hints: false
+    },
+    mode: "production",
+    devServer: {
+      static: './dist',
+      hot: true
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        title: "p5 sketch"
+      })
     ],
-  },
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true
-  },
-  optimization: {
-    runtimeChunk: 'single',
-    minimize: false,
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all',
+    module: {
+      rules: [
+        {
+          test: /\.p5.js$/,
+          use: [
+            {
+              loader: path.resolve('p5-global-loader.js')
+            }
+          ]
+        },
+      ],
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true
+    },
+    optimization: {
+      runtimeChunk: 'single',
+      minimize: false,
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            chunks: 'all',
+          },
         },
       },
     },
-  },
+  }
 }
